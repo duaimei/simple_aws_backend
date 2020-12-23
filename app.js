85a73a52dad7b1a24289202bcff1da1414b74f61
duaimei <adetwiler@optoro.com>
2020-12-23 12:04:30 -0500

    I don't remember why I did this

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors({
   // origin: 'http://localhost:8080'
-  origin: [process.env.APP_ADMIN, process.env.MAIN_APP],
+  origin: [process.env.APP_ADMIN, process.env.MAIN_APP, 'http://localhost:8181'],
 }));
 
 
